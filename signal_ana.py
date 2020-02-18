51d989bb10d96c8bfa86184832a3d1533f40e67c
llayer <llayer@cern.ch>
2020-02-18 20:48:46 +0100

    implement clustering

--- signal_ana.py
+++ signal_ana.py
@@ -10,6 +10,10 @@ import skopt
 from skopt.utils import use_named_args
 from skopt import gp_minimize
 import math
+from sklearn.manifold import TSNE  
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from random import randint
 
 
 def load_signals(path, wf_len = 259):
@@ -86,9 +90,9 @@ def get_features(df, wf=True, lpc=False, amp=False):
     return features.to_numpy()
     
     
-def run_som(features, size, niter = 10000, sigma=0.3, learning_rate=.5, pca=True, plot_error = False, random_seed = 1):
+def run_som(features, size_x, xize_y, niter = 10000, sigma=0.3, learning_rate=.5, pca=True, plot_error = False, random_seed = 1):
     
-    som = MiniSom(size, size, features.shape[1], sigma=sigma, learning_rate=learning_rate, random_seed = random_seed) 
+    som = MiniSom(size_x, xize_y, features.shape[1], sigma=sigma, learning_rate=learning_rate, random_seed = random_seed) 
     if pca == True:
         som.pca_weights_init(features)
     else:
@@ -125,12 +129,12 @@ def run_som(features, size, niter = 10000, sigma=0.3, learning_rate=.5, pca=True
     return som
     
     
-def plot_waveforms(features, som, size):
+def plot_waveforms(features, som, size_x, size_y):
     
     win_map = som.win_map(features)
 
     plt.figure(figsize=(14, 14))
-    the_grid = GridSpec(size, size)
+    the_grid = GridSpec(size_x, size_y)
     for position in win_map.keys():
     
         ax = plt.subplot(the_grid[position[0], position[1]])
@@ -143,17 +147,17 @@ def plot_waveforms(features, som, size):
     plt.show()
     
 
-def plot_dmap(features, som, size):
+def plot_dmap(features, som, size_x, size_y):
     
     cmap = cm.get_cmap('bone_r') #gray_r')
     win_map = som.win_map(features)
     
     plt.figure(figsize=(14, 14))
-    the_grid = GridSpec(size, size)
+    the_grid = GridSpec(size_x, size_y)
 
     dmap = som.distance_map() 
-    for i in range(size):
-        for j in range(size):
+    for i in range(size_x):
+        for j in range(size_y):
 
             ax = plt.subplot(the_grid[i, j])
             c = cmap(dmap[i, j])
@@ -167,15 +171,15 @@ def plot_dmap(features, som, size):
     plt.show()
     
 
-def plot_density(features, som, size):
+def plot_density(features, som, size_x, size_y):
     
     win_map = som.win_map(features)
     
     def density_map():
     
-        dm = np.zeros((size, size))
-        for i in range(size):
-            for j in range(size):
+        dm = np.zeros((size_x, size_y))
+        for i in range(size_x):
+            for j in range(size_y):
                 dm[i,j] = len(win_map[i,j])
         return dm/dm.max()
     
@@ -184,10 +188,10 @@ def plot_density(features, som, size):
     cmap = cm.get_cmap('viridis') #gray_r')
     
     plt.figure(figsize=(14, 14))
-    the_grid = GridSpec(size, size)
+    the_grid = GridSpec(size_x, size_y)
 
-    for i in range(size):
-        for j in range(size):
+    for i in range(size_x):
+        for j in range(size_y):
 
             ax = plt.subplot(the_grid[i, j])
             c = cmap(density[i, j])
@@ -213,9 +217,9 @@ def get_erruption_time():
     err2 = h.iloc[1]['time']
     return err1, err2
     
-    
-def plot_evo(df, som, size, wf = True, lpc = True, amp = True):
-    
+
+def feat_to_cluster(df, som, size_x, size_y, wf = True, lpc = True, amp = True):
+
     def to_cluster(waveform, lpc_coeff, amplitude):
         
         feat = []
@@ -228,14 +232,17 @@ def plot_evo(df, som, size, wf = True, lpc = True, amp = True):
         return som.winner(np.array(feat))
 
     def to_index(cluster):
-        index = size * cluster[0] + cluster[1]
+        index = size_y * cluster[0] + cluster[1]
         return index
     
     df['cluster'] = df.apply(lambda x: to_cluster(x['waveform'], x['lpc_coeff'], x['amplitude']), axis=1)
     df['cluster_index'] = df['cluster'].apply(to_index)
     df['time_int'] = df.time.astype(np.int64)
+    
+    
+def plot_evo(df, col, n_clusters):
 
-    x, y = df['time_int'], df['cluster_index']
+    x, y = df['time_int'], df[col]
 
     # Scatter plot
     fig, ax = plt.subplots(figsize=(10,6))
@@ -249,7 +256,7 @@ def plot_evo(df, som, size, wf = True, lpc = True, amp = True):
     delta = d1 - d0
     days = delta.days 
     k = kde.gaussian_kde([x,y])
-    bins_y = size*size
+    bins_y = n_clusters
     bins_x = days
     xi, yi = np.mgrid[x.min():x.max():bins_x*1j, y.min():y.max():bins_y*1j]
     zi = k(np.vstack([xi.flatten(), yi.flatten()]))
@@ -265,7 +272,47 @@ def plot_evo(df, som, size, wf = True, lpc = True, amp = True):
     
     plt.show()
 
-def optimize( dimensions, initial_param, features, size, num_calls=12, pca = False, random_seed = 1): 
+
+def plot_hist2d(df, col, n_clusters):
+
+    x, y = df['time_int'], df[col]
+
+    heatmap, xedges, yedges = np.histogram2d(x, y, bins=(200, n_clusters))
+    extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]+1]
+    
+    # 2d histo
+    fig = plt.figure(1, figsize=(15, 10))
+    ax = fig.add_subplot(111)
+    plt.imshow(heatmap.T, extent=extent, aspect='auto', interpolation = 'nearest')
+
+    err1, err2 = get_erruption_time()
+    ax.axvline(x=err1, color='r', linestyle='dashed', linewidth=2)
+    ax.axvline(x=err2, color='r', linestyle='dashed', linewidth=2)
+    ax.set_xticklabels([datetime.fromtimestamp(ts / 1e9).strftime('%D') for ts in ax.get_xticks()])
+    
+    plt.colorbar()
+    plt.show()
+
+    # Fractions in bin
+    frac_per_bin = []
+    for b in heatmap:
+        fraction = []
+        for cluster in b:
+            if sum(b) > 0:
+                fraction.append(float(cluster) / float(sum(b)))
+            else:
+                fraction.append(0.)
+        frac_per_bin.append(fraction)
+    
+    fig = plt.figure(1, figsize=(12, 8))
+    ax = fig.add_subplot(111)
+    frac_per_bin = np.array(frac_per_bin)
+    for i in range(frac_per_bin.shape[1]):
+        vec = frac_per_bin[:,i]
+        plt.plot(vec, linewidth=2)
+    plt.show()
+    
+def optimize( dimensions, initial_param, features, size_x, xize_y, num_calls=12, pca = False, random_seed = 1): 
 
     prior_values = []
     prior_names = []
@@ -288,7 +335,7 @@ def optimize( dimensions, initial_param, features, size, num_calls=12, pca = Fal
         print(p)
 
         # Train som
-        som = run_som(features, size, niter = int(p['niter']), sigma=p['sigma'], learning_rate=p['learning_rate'], 
+        som = run_som(features, size_x, xize_y, niter = int(p['niter']), sigma=p['sigma'], learning_rate=p['learning_rate'], 
               pca=pca, random_seed = random_seed)
         
         q_error = som.quantization_error(features)
@@ -325,4 +372,129 @@ def optimize( dimensions, initial_param, features, size, num_calls=12, pca = Fal
     return result
 
 
+def run_tsne(features, perplexity = 40, plot=True):
+    
+    #pca_50 = PCA(n_components=50)
+    # low early_ex looks good
+    #pca_result_50 = pca_50.fit_transform(list(features))
+    tsne_model = TSNE(perplexity=perplexity, #early_exaggeration = 500, # learning_rate = 500.00, early_exaggeration = 50,
+                      n_components=2, init='pca', n_iter=1000, random_state=23, verbose=2)
+
+    tsne_values = tsne_model.fit_transform(list(features))
+    x = tsne_values[:,0]
+    y = tsne_values[:,1]
+    
+    if plot == True:
+        
+        plt.figure(figsize=(10,10))
+        plt.scatter(x,y, s=1)
+        plt.show()
+    
+    return x,y
+
+    
+def run_kmeans(features, n_clusters=4):
+    
+    kmeans = KMeans(n_clusters=n_clusters, random_state=0)
+    preds = kmeans.fit_predict(features)
+    return preds
+
+
+def elbow(max_clusters, features):
+    
+    wcss = []
+    for i in range(1, max_clusters):
+        kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
+        kmeans.fit(features)
+        wcss.append(kmeans.inertia_)
+    plt.plot(range(1, max_clusters), wcss)
+    plt.title('Elbow Method')
+    plt.xlabel('Number of clusters')
+    plt.ylabel('WCSS')
+    plt.show()
+
+
+def kmeans_som(som, features, size_x, size_y, n_clusters=4, run_elbow = False):
+    
+    weights = som.get_weights()
+    weights = weights.reshape(size_x * size_y, weights.shape[2])
+    
+    if run_elbow == False:
+        kmeans = KMeans(n_clusters=n_clusters, random_state=0)
+        pred_weights = kmeans.fit_predict(weights)
+
+        weights = weights.reshape(size_x , size_y, weights.shape[1])
+        pred_weights = pred_weights.reshape(size_x , size_y,)
+
+        #colors = 'rgbcy'
+        if n_clusters < 8:
+            colors = 'rgbcymk'
+        else:
+            colors = []
+            for i in range(n_clusters):
+                colors.append('#%06X' % randint(0, 0xFFFFFF))
+            
+        win_map = som.win_map(features)
+        plt.figure(figsize=(14, 14))
+        the_grid = GridSpec(size_x, size_y)
+
+        for i in range(size_x):
+            for j in range(size_y):
+
+                ax = plt.subplot(the_grid[i, j])
+                c = pred_weights[i,j]
+                ax.set_facecolor(colors[c])
+                ax.get_xaxis().set_visible(False)
+                ax.get_yaxis().set_visible(False)
+
+                if (i, j) in win_map.keys():
+                    plt.plot(np.mean(win_map[i, j], axis=0))
+
+        plt.show()
+        
+        return pred_weights
+        
+    else:
+        elbow(10, weights)
+    
+    
+def som_to_kmeans_cluster(df, preds):
+    
+    def to_kmeans( cluster ):
+        kmeans_cluster = preds[cluster]
+        return kmeans_cluster
+    
+    df['cluster_som_kmeans'] = df['cluster'].apply(to_kmeans)
+    
+    
+    
+def plot_kmeans_tsne(df, col, n_clusters, plot_avg = True):
+    
+    if n_clusters < 8:
+        colors = 'rgbcymk'
+    else:
+        colors = []
+        for i in range(n_clusters):
+            colors.append('#%06X' % randint(0, 0xFFFFFF))
+
+    plt.figure(figsize=(10,10))
+    for i in range(n_clusters):
+
+        cls = df[df[col]==i]
+        plt.scatter(cls['tsne_x'], cls['tsne_y'], c = colors[i], s = 5)
+    
+    plt.show()
+    
+    if plot_avg == True:
+        for i in range(n_clusters):
+
+            cls = df[df[col]==i]
+            print(colors[i])
+            plt.figure(figsize=(3,2))
+            for vec in cls['waveform']:
+                plt.plot(vec, color='gray', alpha=.01)
+            plt.plot(np.mean(cls['waveform'], axis=0))
+            plt.show()
+    
+    
     
